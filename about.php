e1be55a4ba180b70ab94906bb25a37807efb9bbb
Roman-Sanchez <romanfsanchez@gmail.com>
2016-01-17 00:41:51 -0700

    added the about webpage

---
+++ about.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>About me</title>
+	<title>Homepage</title>
+	<link rel="stylesheet" type="text/css" href="homepage.css">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+	  	<div class="container-fluid">
+	    	<div class="navbar-header">
+	      	<a class="navbar-brand" href="index.php">Roman Sanchez</a>
+	    </div>
+	    <ul class="nav navbar-nav">
+	      <li><a href="index.php">Home</a></li>
+	      <li><a href="assignments.php">Assignments</a></li>
+	      <li><a href="#">Coding Samples</a></li> 
+	      <li class="active"><a href="#">About</a></li> 
+	    </ul>
+	  </div>
+	</nav>
+	<div>
+		<p>
+			A little bit about me. Gaming, programming, 3D modeling (Zbrush), outdoors, fishing, <br/> 
+			and shooting are some of my hobbies. 
+		</p>
+	</div>
+
+</body>
+</html>
