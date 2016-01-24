08a00d94d26f26a0594bed8cb9f9d8e45569de8c
Roman-Sanchez <romanfsanchez@gmail.com>
2016-01-23 20:12:49 -0700

    Competed the survey assignment

---
+++ Survey Assignment/survey.php
@@ -0,0 +1,84 @@
+<?php
+	
+	/*$comedy = $_POST["q1"];
+	$fantasy = $_POST["q2"];
+	$action = $_POST["q3"];
+	$guyFilm = $_POST["q4"];
+
+	$movieData = array($comedy, $fantasy, $action, $guyFilm);*/
+	
+	// check to see if the method that was sent is a POST
+	// step through each answer and add it append them to the results file
+	if ($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+		$comedy = $_POST["q1"];
+		$fantasy = $_POST["q2"];
+		$action = $_POST["q3"];
+		$guyFilm = $_POST["q4"];
+
+		$movieData = array($comedy, $fantasy, $action, $guyFilm);
+		foreach ($movieData as $movie)
+		{
+			if (file_exists("results.txt"))
+			{
+				file_put_contents("results.txt", "," . $movie, FILE_APPEND | LOCK_EX);
+			}
+			else
+				file_put_contents("results.txt", $movie, FILE_APPEND | LOCK_EX);
+		}
+	}
+
+	// read file contents into a string
+	$results = file_get_contents("results.txt");
+
+	// count how many times each movie is in the results file
+	$numUHF = substr_count($results, "UHF");
+	$numNutty = substr_count($results, "Nutty Professor");
+	$numKrull = substr_count($results, "Krull");
+	$numManos = substr_count($results, "Manos the hands of fate");
+	$numMadMax = substr_count($results, "Mad Max: Fury Road");	
+	$numAliens = substr_count($results, "Aliens");
+	$numDirty = substr_count($results, "Dirty Harry");
+	$numFather = substr_count($results, "The Godfather");
+
+	// total number of votes for each category, these values will 
+	$totalComedy = $numUHF + $numNutty;
+	$totalFantasy = $numKrull + $numManos;
+	$totalAction = $numMadMax + $numAliens;
+	$totalGuyFilm = $numDirty + $numFather;
+
+
+	$total = $numUHF + $numNutty + $numMadMax + $numAliens + $numDirty 
+	+ $numFather + $numKrull + $numManos;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Results</title>
+</head>
+<body>
+<h1>Survey results</h1><br/>
+<div>
+	
+	<?php
+		echo "<h3>Best Comedy</h3>";
+		echo "Nutty Professor $numNutty votes<br/>";
+		echo "UHF $numUHF votes<br/>";
+
+		echo "<h3>Best Fantasy</h3>";
+		echo "Krull $numKrull votes<br/>";
+		echo "Manos the hands of fate $numManos votes<br/>";
+
+		echo "<h3>Best Action</h3>";
+		echo "Mad Max: Fury Road $numMadMax votes<br/>";
+		echo "Aliens $numAliens votes<br/>";
+
+		echo "<h3>Best Guy Film</h3>";
+		echo "Dirty Harry $numDirty votes<br/>";
+		echo "The Godfather $numFather votes<br/>";
+	?>
+	
+</div>
+</body>
+</html>
---
+++ Survey Assignment/surveyHome.php
@@ -0,0 +1,71 @@
+<?php
+  $COMPLETED_KEY = "surveySubmitted";
+  $completedSurvey = true;
+  if (isset($_COOKIE[$COMPLETED_KEY])) 
+  {
+    //header( 'Location: ' ) ;
+  }
+  else
+    setcookie($COMPLETED_KEY, $completedSurvey);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>PHP SURVEY</title>
+
+    <!-- Bootstrap -->
+    <link href="../bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div>
+      <h1>Movie Survey</h1>
+
+      <div>
+        <div class="form-group">
+          <form action="survey.php" method="POST">
+            <div class="">Best comedy film</div>
+            <div><input type="radio" name="q1" id="q1" value="UHF" checked="checked">UHF</div>
+            <div><input type="radio" name="q1" id="q1" value="Nutty Professor">Nutty Professor (1963)</div>
+            <br/>
+
+            <div class="">Best fantasy film</div>
+            <div><input type="radio" name="q2" id="q2" value="Krull" checked="checked">Krull</div>
+            <div><input type="radio" name="q2" id="q2" value="Manos the hands of fate">Manos the hands of fate</div>
+            <br/>
+
+            <div class="">Best action film</div>
+            <div><input type="radio" name="q3" id="q3" value="Mad Max: Fury Road" checked="checked">Mad Max: Fury Road</div>
+            <div><input type="radio" name="q3" id="q3" value="Aliens">Aliens</div>
+            <br/>
+
+            <div class="">Best guy film</div>
+            <div><input type="radio" name="q4" id="q4" value="Dirty Harry" checked="checked">Dirty Harry</div>
+            <div><input type="radio" name="q4" id="q4" value="The Godfather">The Godfather</div>
+            <br/>
+
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div>
+      <a href="survey.php"> See results </a>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
