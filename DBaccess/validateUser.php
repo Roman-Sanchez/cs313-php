6d5e57532e58de577cc28564179b6dea6b615f35
Roman-Sanchez <romanfsanchez@gmail.com>
2016-02-06 17:18:51 -0700

    adding openshift environment variables

--- DBaccess/validateUser.php
+++ DBaccess/validateUser.php
@@ -7,11 +7,30 @@
 		$_SESSION["userName"] = $userName;
 		$_SESSION["pWord"] = $pWord;
 
-		var_dump($_SESSION["userName"]);
 
+		$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+
+		if ($openShiftVar == null || $openShiftVar == "")
+		{
+		    // Not in the openshift environment
+		    $dbName = 'movietracker';
+		    $dbHost = 'localhost'; 
+			// not need just yet $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+			$dbUser = 'romanfs'; 
+			$dbPassword = 'password';
+		}
+		else 
+		{
+		    // In the openshift environment 
+			$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
+			$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT'); 
+			$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); 
+			$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+			echo "host:$dbHost:$dbPort user:$dbUser password:$dbPassword<br />\n";
+		}
 
 		try{
-			$db = new PDO('mysql:host=127.0.0.1;dbname=movietracker', 'root', '');
+			$db = new PDO('mysql:host=dbHost:dbPort;dbname=php', $dbUser, $dbPassword);
 		}
 		catch (PDOException $ex) 
 		{
